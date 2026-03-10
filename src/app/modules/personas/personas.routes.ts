3f230318031f5c099f26050bdfa96d85b4728d65
martinOfi <martin@tqr.la>
2026-03-10 17:43:03 -0300

    refactor(personas): remove standalone list pages for protagonistas, educadores, personas-externas
    The dashboard now serves as the unified list view for all persona types.
    Removed redundant list page components and their sub-components (tables,
    filters, empty states) while preserving form and detail components.

--- src/app/modules/personas/personas.routes.ts
+++ src/app/modules/personas/personas.routes.ts
@@ -17,11 +17,6 @@ export const PERSONAS_ROUTES: Routes = [
     path: 'protagonistas',
     children: [
       {
-        path: '',
-        loadComponent: () => import('./pages/protagonistas-page/protagonistas-page.component')
-          .then(m => m.ProtagonistasPageComponent)
-      },
-      {
         path: 'crear',
         loadComponent: () => import('./components/protagonistas/form/protagonistas-form.component')
           .then(m => m.ProtagonistasFormComponent)
@@ -44,11 +39,6 @@ export const PERSONAS_ROUTES: Routes = [
     path: 'educadores',
     children: [
       {
-        path: '',
-        loadComponent: () => import('./pages/educadores-page/educadores-page.component')
-          .then(m => m.EducadoresPageComponent)
-      },
-      {
         path: 'crear',
         loadComponent: () => import('./components/educadores/form/educadores-form.component')
           .then(m => m.EducadoresFormComponent)
@@ -70,11 +60,6 @@ export const PERSONAS_ROUTES: Routes = [
   {
     path: 'personas-externas',
     children: [
-      {
-        path: '',
-        loadComponent: () => import('./pages/personas-externas-page/personas-externas-page.component')
-          .then(m => m.PersonasExternasPageComponent)
-      },
       {
         path: 'crear',
         loadComponent: () => import('./components/personas-externas/form/personas-externas-form.component')
